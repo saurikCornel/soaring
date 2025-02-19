29335cd2f262a921b37cc565f7bb370c08fce610
Alex Belov <alex@cornel.su>
2025-02-19 14:27:08 +0300

    Now

--- Soaring Eagles Game/RotateDeviceScreen.swift
+++ Soaring Eagles Game/RotateDeviceScreen.swift
@@ -6,12 +6,22 @@ import SwiftUI
 struct RotateDeviceScreen: View {
     var body: some View {
         GeometryReader { geometry in
-            Image(systemName: "rectangle.landscape.rotate")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(.white)
-                .padding()
-                .shadow(radius: 10)
+            VStack {
+                Image(systemName: "rectangle.landscape.rotate")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.white)
+                    .padding()
+                    .shadow(radius: 10)
+                Text("To continue, rotate the device screen to horizontal orientation")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .shadow(radius: 20)
+                    .padding()
+                    .background(Color.black.opacity(0.5))
+                    .cornerRadius(20)
+                    
+            }
                 .frame(width: geometry.size.width)
                 .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
             
